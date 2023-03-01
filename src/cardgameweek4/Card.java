873f6fed69163964a60668c646d2883d06afffd2
dannyelzein <elzeindanny5@gmail.com>
2023-03-01 18:55:27 -0500

    first commit

---
+++ src/cardgameweek4/Card.java
@@ -0,0 +1,35 @@
+package cardgameweek4;
+
+/**
+ * Starter code for the Card class. To be used in Week 4.
+ *
+ * @author dancye, 2019
+ * @modified Paul Bonenfant Feb 2022
+ * @modified Danny Elzein/Isra Sookun March 2023
+ */
+public class Card {
+
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    }
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+}
